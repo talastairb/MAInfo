8b78320b24178a7f90f13b30c96817b3f692e81d
talastairb <alastair@alastair.milton.edu>
2014-04-14 13:00:34 -0400

    Initial Commit

---
+++ MAInfoMaster/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MAInfoMaster
+//
+//  Created by Alastair on 4/14/14.
+//  Copyright (c) 2014 Alastair. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
